665bcf86672bf3f51d78666bfa00c50ec40b0a13
Jake Walsh <jake.donigan.walsh@monsanto.com>
2020-12-08 10:37:02 -0600

    added linked list to data structures -jw

---
+++ data_structures/linked_list.go
@@ -0,0 +1,77 @@
+package data_structures
+
+type Node struct {
+	Value int
+	Next *Node
+}
+
+type LinkedList struct {
+	n *Node
+	count int
+}
+
+func (l *LinkedList) Add(value int) {
+	if l == nil {
+		return
+	}
+	node := &Node{Value: value}
+	l.count++
+	if l.n == nil {
+		l.n = node
+		return
+	}
+	n := l.n
+	for n.Next != nil {
+		n = n.Next
+	}
+	n.Next = node
+}
+
+func (l *LinkedList) DeleteValue(value int) {
+	if l.n == nil {
+		return
+	}
+	n := l.n
+	if l.n.Value == value {
+		l.n = n.Next
+		l.count--
+	}
+	for n.Next != nil {
+		if n.Next.Value == value {
+			l.count--
+			n.Next = n.Next.Next
+			continue
+		}
+		n = n.Next
+	}
+	if l.count == 0 {
+		*l = LinkedList{}
+	}
+}
+
+func (l *LinkedList) DeleteIndex(index int) {
+	if index > l.count -1 {
+		return
+	}
+	n := l.n
+	if index == 0 {
+		l.count --
+		l.n = n.Next
+		return
+	}
+	count := 0
+	for n.Next != nil {
+		count++
+		if count == index {
+			l.count--
+			n.Next = n.Next.Next
+			return
+		}
+		n = n.Next
+	}
+}
+
+func (l *LinkedList) Empty() bool {
+	return l == nil || l.count == 0
+}
+
---
+++ data_structures/linked_list_test.go
@@ -0,0 +1,119 @@
+package data_structures
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestLinkedList_Add(t *testing.T) {
+	t.Run("adds element when list is not nil", func(t *testing.T) {
+		expected := LinkedList{n: &Node{Value: 5}, count: 1}
+
+		var testObject LinkedList
+		testObject.Add(5)
+		assert.Equal(t, expected, testObject)
+	})
+	t.Run("adds multiple elements", func(t *testing.T) {
+		expected := LinkedList{n: &Node{Value: 5, Next: &Node{Value: 10}}, count: 2}
+
+		var testObject LinkedList
+		testObject.Add(5)
+		testObject.Add(10)
+		assert.Equal(t, expected, testObject)
+	})
+}
+
+func TestLinkedList_Empty(t *testing.T) {
+	t.Run("is empty when l is nil", func(t *testing.T) {
+		var testObject *LinkedList
+		assert.True(t, testObject.Empty())
+	})
+	t.Run("is empty when l has no elements", func(t *testing.T) {
+		testObject := LinkedList{}
+		assert.True(t, testObject.Empty())
+	})
+	t.Run("is not empty after adding element", func(t *testing.T) {
+		testObject := LinkedList{}
+		testObject.Add(5)
+		assert.False(t, testObject.Empty())
+	})
+}
+func TestLinkedList_DeleteValue(t *testing.T) {
+	t.Run("does not delete when cannot find value", func(t *testing.T) {
+		expected := LinkedList{n: &Node{Value: 5, Next: &Node{Value: 10}}, count: 2}
+
+		var testObject LinkedList
+		testObject.Add(5)
+		testObject.Add(10)
+
+		testObject.DeleteValue(20)
+		assert.Equal(t, expected, testObject)
+	})
+	t.Run("deletes all values that occur", func(t *testing.T) {
+		expected := LinkedList{n: &Node{Value: 5}, count: 1}
+
+		var testObject LinkedList
+		testObject.Add(10)
+		testObject.Add(5)
+		testObject.Add(10)
+
+		testObject.DeleteValue(10)
+		assert.Equal(t, expected, testObject)
+	})
+	t.Run("deletes all values that occur", func(t *testing.T) {
+		expected := LinkedList{}
+
+		var testObject LinkedList
+		testObject.Add(10)
+		testObject.Add(10)
+		testObject.Add(10)
+
+		testObject.DeleteValue(10)
+		assert.Equal(t, expected, testObject)
+	})
+}
+func TestLinkedList_DeleteIndex(t *testing.T) {
+	t.Run("does not delete when cannot find index", func(t *testing.T) {
+		expected := LinkedList{n: &Node{Value: 5, Next: &Node{Value: 10}}, count: 2}
+
+		var testObject LinkedList
+		testObject.Add(5)
+		testObject.Add(10)
+
+		testObject.DeleteIndex(5)
+		assert.Equal(t, expected, testObject)
+	})
+	t.Run("deletes middle index", func(t *testing.T) {
+		expected := LinkedList{n: &Node{Value: 10, Next: &Node{Value: 10}}, count: 2}
+
+		var testObject LinkedList
+		testObject.Add(10)
+		testObject.Add(5)
+		testObject.Add(10)
+
+		testObject.DeleteIndex(1)
+		assert.Equal(t, expected, testObject)
+	})
+	t.Run("deletes first index", func(t *testing.T) {
+		expected := LinkedList{n: &Node{Value: 5, Next: &Node{Value: 10}}, count: 2}
+
+		var testObject LinkedList
+		testObject.Add(10)
+		testObject.Add(5)
+		testObject.Add(10)
+
+		testObject.DeleteIndex(0)
+		assert.Equal(t, expected, testObject)
+	})
+	t.Run("deletes last index", func(t *testing.T) {
+		expected := LinkedList{n: &Node{Value: 10, Next: &Node{Value: 5}}, count: 2}
+
+		var testObject LinkedList
+		testObject.Add(10)
+		testObject.Add(5)
+		testObject.Add(10)
+
+		testObject.DeleteIndex(2)
+		assert.Equal(t, expected, testObject)
+	})
+}
