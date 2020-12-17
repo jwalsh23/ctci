11949e1cc1742efc4a84e41afb7e367d8e7a2e1a
Jake Walsh <jake.donigan.walsh@monsanto.com>
2020-12-17 10:27:48 -0600

    added logic for palindrome detection with two pointer approach -jw

--- chapter2/exercise2_6.go
+++ chapter2/exercise2_6.go
@@ -1,6 +1,8 @@
 package chapter2
 
-import "github.com/jwalsh23/ctci/data_structures"
+import (
+	"github.com/jwalsh23/ctci/data_structures"
+)
 
 func IsPalindrome(list data_structures.LinkedList) bool {
 	valStack := data_structures.Stack{}
@@ -15,6 +17,27 @@ func IsPalindrome(list data_structures.LinkedList) bool {
 			return false
 		}
 		n = n.Next
+	}
+	return true
+}
+
+func IsPalindromeTwoPointer(list data_structures.LinkedList) bool {
+	fast, slow := list.N, list.N
+	stack := data_structures.Stack{}
+	for fast != nil && fast.Next != nil {
+		stack.Push(slow.Value)
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	// this is for odd lists
+	if fast != nil {
+		slow = slow.Next
+	}
+	for slow != nil {
+		if slow.Value != stack.Pop() {
+			return false
+		}
+		slow = slow.Next
 	}
 	return true
 }
--- chapter2/exercise2_6_test.go
+++ chapter2/exercise2_6_test.go
@@ -30,3 +30,36 @@ func TestIsPalindrome(t *testing.T) {
 		assert.True(t, chapter2.IsPalindrome(testObject))
 	})
 }
+
+func TestIsPalindromeTwoPointer(t *testing.T) {
+	t.Run("is not", func(t *testing.T) {
+		testObject := data_structures.LinkedList{}
+		testObject.Add(5)
+		testObject.Add(4)
+		testObject.Add(4)
+		testObject.Add(6)
+
+		assert.False(t, chapter2.IsPalindromeTwoPointer(testObject))
+	})
+	t.Run("is palindrome even", func(t *testing.T) {
+		testObject := data_structures.LinkedList{}
+		testObject.Add(1)
+		testObject.Add(5)
+		testObject.Add(4)
+		testObject.Add(4)
+		testObject.Add(5)
+		testObject.Add(1)
+
+		assert.True(t, chapter2.IsPalindromeTwoPointer(testObject))
+	})
+	t.Run("is palindrome odd", func(t *testing.T) {
+		testObject := data_structures.LinkedList{}
+		testObject.Add(1)
+		testObject.Add(5)
+		testObject.Add(6)
+		testObject.Add(5)
+		testObject.Add(1)
+
+		assert.True(t, chapter2.IsPalindromeTwoPointer(testObject))
+	})
+}
